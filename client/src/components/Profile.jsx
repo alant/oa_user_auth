6ab110a650c54fbae40279ec9dfad4798ad24426
alant <alan.htang@gmail.com>
2019-11-12 07:28:22 -0800

    adding the profile file

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import Header from "./Header";
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <div>
+            <h1>profile page</h1>
+        </div>
+      </div>
+    );
+  }
+
+}
