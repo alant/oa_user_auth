82eab2b09c38a78cca7d8a89fb32b35ecd142542
alant <alan.htang@gmail.com>
2019-11-12 07:33:05 -0800

    Updates

---
+++ precache-manifest.a88b70d7023d293ed21e4e7293af68ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917a776570315ba74819eaccc0b41221",
+    "url": "/oa_user_auth/index.html"
+  },
+  {
+    "revision": "172c20eb5186dbd8a310",
+    "url": "/oa_user_auth/static/css/main.1369990b.chunk.css"
+  },
+  {
+    "revision": "467f502dbd63475b54fc",
+    "url": "/oa_user_auth/static/js/2.0f976489.chunk.js"
+  },
+  {
+    "revision": "172c20eb5186dbd8a310",
+    "url": "/oa_user_auth/static/js/main.7fc4171c.chunk.js"
+  },
+  {
+    "revision": "f0e636747b3205488977",
+    "url": "/oa_user_auth/static/js/runtime~main.8553a37d.js"
+  }
+]);
