64f380500108d7b4749b3626aaf7aadfc13ee7d5
alant <alan.htang@gmail.com>
2019-11-18 08:57:11 -0800

    adding semantic-ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,44 @@
 import { Link } from "react-router-dom";
 import React from "react";
 import { AuthContext } from "../App";
+import { Sticky } from 'semantic-ui-react';
 
 function Header() {
   const { dispatch, state } = React.useContext(AuthContext);
 
   return (
-    <ul className="menu">
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/profile">Profile</Link>
-      </li>
-        {!state.isAuthenticated ? 
-          <Link to="/login">Login</Link>
-          :
-          <button className="link" onClick={handleLogOutClick}>Logout</button>
-        }
-    </ul>
+    <Sticky>
+      <div class="ui secondary pointing menu">
+        <a class="item">
+          Home
+        </a>
+        <a class="item">
+          Messages
+        </a>
+        <a class="item">
+          Friends
+        </a>
+        <div class="right menu">
+          <a class="ui item">
+            Logout
+          </a>
+        </div>
+
+      </div>
+    </Sticky>
+    // <ul className="menu">
+    //   <li>
+    //     <Link to="/">Home</Link>
+    //   </li>
+    //   <li>
+    //     <Link to="/profile">Profile</Link>
+    //   </li>
+    //     {!state.isAuthenticated ? 
+    //       <Link to="/login">Login</Link>
+    //       :
+    //       <button className="link" onClick={handleLogOutClick}>Logout</button>
+    //     }
+    // </ul>
   );
 
   function handleLogOutClick() {
