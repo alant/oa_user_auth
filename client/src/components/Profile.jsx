ea56c439618229ddce3e1ea634855567059a28aa
alant <alan.htang@gmail.com>
2019-11-28 11:43:02 -0800

    moving react to docker

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -4,7 +4,7 @@ export default class Profile extends Component {
   render() {
     return (
       <div>
-        <h1>profile page</h1>
+        <h1>profile page under auth</h1>
       </div>
     );
   }
